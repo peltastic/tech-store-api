ecebd17027e8a4f8a72c759825a8b4fc1e727591
peltastic <onosogapelumi@gmail.com>
2022-04-25 22:26:19 +0100

    changes

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require("cors")
-const dotenv = require('dotenv')
-const bodyparser = require('body-parser');
-const authRoutes = require('./api/routes/auth')
-const cartRoutes = require('./api/routes/carts')
-const productsRoutes = require('./api/routes/products')
-const orderRoutes = require('./api/routes/orders')
-const checkoutRoutes = require('./api/routes/checkout')
-dotenv.config()
-const DB = require('./db')
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}))
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyparser = require("body-parser");
+const authRoutes = require("./api/routes/auth");
+const cartRoutes = require("./api/routes/carts");
+const productsRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+const checkoutRoutes = require("./api/routes/checkout");
+dotenv.config();
+const DB = require("./db");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
-DB.init()
+DB.init();
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.use('/auth', authRoutes)
-app.use('/products', productsRoutes)
-app.use('/cart', cartRoutes)
-app.use('/order', orderRoutes)
-app.use('/checkout', checkoutRoutes)
+app.use("/auth", authRoutes);
+app.use("/products", productsRoutes);
+app.use("/cart", cartRoutes);
+app.use("/order", orderRoutes);
+app.use("/checkout", checkoutRoutes);
 
-app.listen(process.env.PORT ||8000, () => console.log('server is running'))
+app.listen(process.env.PORT || 8000, () => console.log("server is running"));
