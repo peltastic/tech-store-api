129d088747aae787d166ee6f4d22b43a05ebd6d2
peltastic <onosogapelumi@gmail.com>
2022-05-17 16:37:00 +0100

    update

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 module.exports = {
     JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRATION: `${process.env.JWT_EXPIRATION}h`,
+    JWT_EXPIRATION: `${process.env.JWT_EXPIRATION}m`,
     JWT_EXPIRATION_MS: process.env.JWT_EXPIRATION_MS,
     JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION
 }
